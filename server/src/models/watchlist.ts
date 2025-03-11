0ef7bdca7cbbe0be384690c1d05cf4b9b35765b7
Jordan Meade <jordanmeade451@gmail.com>
2025-03-10 21:04:23 -0400

    remade server.ts and added stockJournal.ts

--- server/src/models/watchlist.ts
+++ server/src/models/watchlist.ts
@@ -1,75 +1,75 @@
-import { DataTypes, Sequelize, Model, Optional } from 'sequelize';
+// import { DataTypes, Sequelize, Model, Optional } from 'sequelize';
 
-interface WatchlistAttributes {
-  watchlistId: number;
-  stockId:number;
-  stockValue: number;
-  stockTicker: string;
-}
+// interface WatchlistAttributes {
+//   watchlistId: number;
+//   stockId:number;
+//   stockValue: number;
+//   stockTicker: string;
+// }
 
-interface WatchlistCreationAttributes extends Optional<WatchlistAttributes, 'watchlistId'> {}
+// interface WatchlistCreationAttributes extends Optional<WatchlistAttributes, 'watchlistId'> {}
 
-export class Watchlist extends Model<WatchlistAttributes, WatchlistCreationAttributes> implements WatchlistAttributes {
-  // public id!: number;
-  // public name!: string;
-  // public status!: string;
-  // public description!: string;
-  // public assignedUserId!: number;
+// export class Watchlist extends Model<WatchlistAttributes, WatchlistCreationAttributes> implements WatchlistAttributes {
+//   // public id!: number;
+//   // public name!: string;
+//   // public status!: string;
+//   // public description!: string;
+//   // public assignedUserId!: number;
 
-  // // associated User model
-  // public readonly assignedUser?: User;
+//   // // associated User model
+//   // public readonly assignedUser?: User;
 
-  // public readonly createdAt!: Date;
-  // public readonly updatedAt!: Date;
+//   // public readonly createdAt!: Date;
+//   // public readonly updatedAt!: Date;
 
-  watchlistId!: number;
-  stockId!:number;
-  stockValue!: number;
-  stockTicker!: string;
-}
+//   watchlistId!: number;
+//   stockId!:number;
+//   stockValue!: number;
+//   stockTicker!: string;
+// }
 
-export function WatchlistFactory(sequelize: Sequelize): typeof Watchlist {
-  Watchlist.init(
-    {
-      watchlistId: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      stockId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      stockValue: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      stockTicker: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      // name: {
-      //   type: DataTypes.STRING,
-      //   allowNull: false,
-      // },
-      // status: {
-      //   type: DataTypes.STRING,
-      //   allowNull: false,
-      // },
-      // description: {
-      //   type: DataTypes.STRING,
-      //   allowNull: false,
-      // },
-      // assignedUserId: {
-      //   type: DataTypes.INTEGER,
-      //   allowNull: true,
-      // },
-    },
-    {
-      tableName: 'watchlist',
-      sequelize,
-    }
-  );
+// export function WatchlistFactory(sequelize: Sequelize): typeof Watchlist {
+//   Watchlist.init(
+//     {
+//       watchlistId: {
+//         type: DataTypes.INTEGER,
+//         autoIncrement: true,
+//         primaryKey: true,
+//       },
+//       stockId: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false
+//       },
+//       stockValue: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false
+//       },
+//       stockTicker: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//       },
+//       // name: {
+//       //   type: DataTypes.STRING,
+//       //   allowNull: false,
+//       // },
+//       // status: {
+//       //   type: DataTypes.STRING,
+//       //   allowNull: false,
+//       // },
+//       // description: {
+//       //   type: DataTypes.STRING,
+//       //   allowNull: false,
+//       // },
+//       // assignedUserId: {
+//       //   type: DataTypes.INTEGER,
+//       //   allowNull: true,
+//       // },
+//     },
+//     {
+//       tableName: 'watchlist',
+//       sequelize,
+//     }
+//   );
 
-  return Watchlist;
-}
+//   return Watchlist;
+// }
